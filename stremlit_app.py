5cefb60b637bf030863fcff903b1eb1236379f69
GunjanLd <gunjan.waghmare@lumendata.com>
2025-04-02 15:15:29 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -34,8 +34,8 @@ ingredients_string= ''
 
 for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen +" "
-    st.subheader(fruit_chosen+' Nutition_information)
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
+    st.subheader(fruit_chosen+' Nutition_information')
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
     sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 st.write(ingredients_string)
 
